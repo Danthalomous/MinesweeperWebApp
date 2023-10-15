eb6ac231d2f4001ee168dde4d8e79c3274d8cbc7
Eli9Saavedra <109545104+Eli9Saavedra@users.noreply.github.com>
2023-10-15 16:34:44 -0700

    Update site.js

--- MinesweeperWebApp/wwwroot/js/site.js
+++ MinesweeperWebApp/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+// Reveal a cell based on its position and update the board
+function revealCell(row, col) {
+    var cellValue = board[row][col];
+    var cellElement = document.querySelector('.game-cell[data-row="' + row + '"][data-col="' + col + '"]');
+    
+    // If the cell contains a mine
+    if (cellValue === 'M') {
+        cellElement.style.backgroundColor = 'red';  // TODO: Replace with mine icon/image
+        // TODO: Implement "Game Over" logic
+    } else if (cellValue === 0) {
+        cellElement.style.backgroundColor = '#ddd';
+        // TODO: Implement recursion to reveal neighboring cells
+    } else {
+        cellElement.innerText = cellValue;
+        cellElement.style.backgroundColor = '#ddd';
+    }
+}
 
-// Write your JavaScript code.
+// Attach event listeners to game cells
+var gameCells = document.querySelectorAll('.game-cell');
+gameCells.forEach(function(cell) {
+    cell.addEventListener('click', function(event) {
+        var clickedCell = event.target;
+        var row = parseInt(clickedCell.getAttribute('data-row'));
+        var col = parseInt(clickedCell.getAttribute('data-col'));
+        revealCell(row, col);
+    });
+});
