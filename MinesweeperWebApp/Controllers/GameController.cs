0da021d42d88199373618b3124909d1bbd607ef5
Eli9Saavedra <109545104+Eli9Saavedra@users.noreply.github.com>
2023-10-15 19:45:03 -0700

    Add files via upload

---
+++ MinesweeperWebApp/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Milestone_Project;
+
+public class GameController : Controller
+{
+    private static Board board = new Board(0.5);
+
+
+    public IActionResult PlayGame()
+    {
+        return View(board.Grid);
+    }
+
+
+    [HttpPost]
+    public IActionResult CellClicked(int row, int col)
+    {
+        board.ProcessClick(row, col);
+        return RedirectToAction("PlayGame");
+    }
+}
